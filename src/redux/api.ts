41409fb22f414bf63c10c982c2cca7b8ca7c6fd1
Son <sondv@wolffungame.com>
2023-05-16 00:10:49 +0700

    add axios

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,11 +1,11 @@
-import {callApi} from '../https/fetch';
+import axiosDefault from '../https/base';
 import {ConfigBanner} from '../models';
 
 export const getBannerDashboard = async () => {
-  const res = await callApi<ConfigBanner>(
-    'https://data.staging.thetanarena.com/theta/v1/mkpdashboard/banner/getlist',
-  );
-  if (res.data?.listBanner && res.success) {
+  const res = await axiosDefault<ConfigBanner>({
+    url: 'https://data.staging.thetanarena.com/theta/v1/mkpdashboard/banner/getlist',
+  });
+  if (res.data?.listBanner && !res.status) {
     return res.data.listBanner;
   }
   return undefined;
